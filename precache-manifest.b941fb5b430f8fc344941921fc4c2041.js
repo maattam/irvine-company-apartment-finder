e48115d120ba350400eb471b8ae07a6b065e1ada
Matti Määttä <matti.maatta@vincit.com>
2019-04-01 20:17:08 -0700

    Updates

---
+++ precache-manifest.b941fb5b430f8fc344941921fc4c2041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a303bde82a6cf2ccdb2",
+    "url": "/irvine-company-apartment-finder/static/js/runtime~main.cbc28444.js"
+  },
+  {
+    "revision": "51fe5a5448f819cf2aac",
+    "url": "/irvine-company-apartment-finder/static/js/main.9877ee39.chunk.js"
+  },
+  {
+    "revision": "511e4090a9f324d24208",
+    "url": "/irvine-company-apartment-finder/static/js/2.5f0f31f1.chunk.js"
+  },
+  {
+    "revision": "51fe5a5448f819cf2aac",
+    "url": "/irvine-company-apartment-finder/static/css/main.5455745a.chunk.css"
+  },
+  {
+    "revision": "54161adf3fd6669a658281d69d0babdc",
+    "url": "/irvine-company-apartment-finder/index.html"
+  }
+];
